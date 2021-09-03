08b087c3cb04d710568bbe58758077f26b583e11
Renuvb <xiejiaqi001@126.com>
2021-09-03 18:46:38 +0800

    add cnn.py
    Change-Id: I44c332aac8206ab8d0a08fadbf0e3d63c28c646d

---
+++ water_melon/chapter_5/cnn.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+class Convo_layer(object):
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+def func_learn():
+    nd_a = np.arange(12).reshape((3,4))
+
+
+
+if __name__ == '__main__':
+    # main()
+    func_learn()

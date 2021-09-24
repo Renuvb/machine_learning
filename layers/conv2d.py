77f8815690960070762590773410dbead666e228
Renuvb <xiejiaqi001@126.com>
2021-09-24 18:46:13 +0800

    fix: conv2d layer bugs
    Change-Id: I76b4f00d929341d1678115e8ef3b8b2edd3e8d11

--- layers/conv2d.py
+++ layers/conv2d.py
@@ -3,6 +3,7 @@
 # from layer import Layer
 from layer import Layer
 import numpy as np
+import os
 
 
 def calc_conv_shape(input_shape, filter_shape, padding=0, stride=1):
@@ -14,8 +15,9 @@ def conv2d(input, filter, stride=1):
     :param input: batch * height * width * channel
     :param filter: filter_number * filter_size * filter_size * channel
     :param stride:
-    :return:
+    :return: batch * output_shape[0] * output_shape[1] * filter_number
     """
+    # print(input.shape, filter.shape)
     output_shape = calc_conv_shape(input.shape[1:3], filter.shape[1:3], stride=stride)
     filter_number = filter.shape[0]
     output = np.zeros((input.shape[0], output_shape[0], output_shape[1], filter_number))
@@ -34,6 +36,8 @@ def padding(input, padding):
     :param padding: int
     :return:
     """
+    if padding == 0:
+        return input
     input_shape = input.shape
     output = np.zeros((input.shape[0], input_shape[1]+padding*2, input_shape[2]+padding*2, input_shape[3]))
     output[:,padding:padding+input_shape[1],padding:padding+input_shape[2],:] = input
@@ -61,7 +65,8 @@ class Conv2d(Layer):
         self.padding = padding
         self.stride = stride
         self.filter_shape = (filter_number, filter_size, filter_size, input_shape[3])
-        self.output_shape = calc_conv_shape(input_shape[1:3], filter_size, padding, stride)
+        conv_output_shape = calc_conv_shape(input_shape[1:3], (filter_size, filter_size), padding, stride)
+        self.output_shape = (None, conv_output_shape[0], conv_output_shape[1], filter_number)
         self.w = np.random.rand(filter_number, filter_size, filter_size, input_shape[3])
         if with_bias:
             self.bias = np.random.rand(filter_number)
@@ -90,32 +95,61 @@ class Conv2d(Layer):
             for all filter:
                 delta_w = input_channel * input_h * input_w * 1   conv   filter_num * out_h * out_w * 1 -> input_channel * w_h * w_w * filter_num
         for all_batch:
-            delta_w = input_channel * input_h * input_w * batch   conv   filter_num * out_h * out_w * batch -> input_channel * w_h * w_w * filter_num / batch
+            delta_w = input_channel * input_h * input_w * batch   conv   filter_num * out_h * out_w * batch -> input_channel * w_h * w_w * filter_num
 
 
         backward:
         input_sensitive = batch * padding_output_h * padding_output_w * filter_num   conv   input_channel * w_h * w_w * filter_num -> batch * input_h * input_w * input_channel
         """
-        padding_out = padding(sensitive, self.filter_size+1)
+        padding_out = padding(sensitive, self.filter_size-1)
         rotated_w = rotate(self.w)
         sensitive_x = conv2d(padding_out, rotated_w)
+        if self.padding > 0:
+            sensitive_x = sensitive_x[:,self.padding:-self.padding, self.padding:-self.padding,:]
+        # print("padding_out %s, rotated_w %s, sensitive shape: %s, output_sensitive shape: %s" % (padding_out.shape, rotated_w.shape, sensitive.shape, sensitive_x.shape))
 
-        swapped_input = np.swapaxes(self.current_input, 0, 3)
+        swapped_input = np.swapaxes(padding(self.current_input, self.padding), 0, 3)
         swapped_sensitive = np.swapaxes(sensitive, 0, 3)
         # stride
         delta_w = conv2d(swapped_input, swapped_sensitive)
-        delta_w = np.swapaxes(delta_w, 0, 3) / self.current_input.shape[0] * self.eta
+        delta_w = np.swapaxes(delta_w, 0, 3) / self.current_input.shape[0] / self.w.size * self.eta * -1
         self.w += delta_w
 
         return sensitive_x
 
 
+def read_data(file_name):
+    def split_line(line):
+        line = line.strip()
+        if line == "":
+            return []
+        items = line.split(' ')
+        return [int(a) for a in items]
+
+    file_path = os.path.join(os.path.dirname(__file__), file_name)
+    with open(file_path) as in_file:
+        line = in_file.readline()
+        shape = split_line(line)
+        item_num = 1
+        for num in shape:
+            item_num *= num
+        data = []
+        while len(data) < item_num:
+            data.extend(split_line(in_file.readline()))
+    return np.array(data).reshape(shape)
+
+
 def test():
-    input = np.arange(36).reshape((2, 3, 3, 2))
-    # print(padding(input, 1))
-    filter = np.ones((1, 2,2,2))
-    # print(conv2d(input, filter, 1))
-    print(rotate(np.arange(4).reshape(1,2,2,1)))
+    # input = np.arange(36).reshape((2, 3, 3, 2))
+    # # print(padding(input, 1))
+    # filter = np.ones((1,2,2,2))
+    # # print(conv2d(input, filter, 1))
+    # print(rotate(np.arange(4).reshape(1,2,2,1)))
+    input = read_data(os.path.join(os.path.dirname(__file__), "../self_data/cnn_input"))
+    if len(input.shape) == 3:
+        input = np.swapaxes(input.reshape(input.shape[0], input.shape[1], input.shape[2], 1), 0, 3)
+    filter = read_data(os.path.join(os.path.dirname(__file__), "../self_data/cnn_filter_nhwc"))
+    print(conv2d(padding(input,1), filter))
 
 
 if __name__ == '__main__':
